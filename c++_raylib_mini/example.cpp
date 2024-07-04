33ad96eda301c7c66601be29fa7ed1e2172fe1e7
Saarang <88927866+saaranganand@users.noreply.github.com>
2024-07-04 12:01:56 -0700

    Update example.cpp

--- c++_raylib_mini/example.cpp
+++ c++_raylib_mini/example.cpp
@@ -19,6 +19,14 @@ int main() {
         DrawText("Congrats! You created your first window!", 120, 200, 20, LIGHTGRAY);
         DrawText("Press ESC to close the window.", 120, 240, 20, LIGHTGRAY);
 
+        if (IsKeyPressed(VK_SPACE)) {
+            DrawText("Space key was just pressed!", 120, 280, 20, LIGHTGRAY);
+        }
+
+        if (IsKeyDown(VK_SPACE)) {
+            DrawText("Space key is being held down!", 120, 320, 20, LIGHTGRAY);
+        }
+        
         EndDrawing();
     }
 
